5df718be5c1b5d38c17ccb218d0565e9daa0c36d
Kawsar Ahmed <coder.kawsar@gmail.com>
2023-12-19 15:56:36 +0600

    Subscription history module accomplished

---
+++ src/app/modules/subscription-history/subscription-history.controller.ts
@@ -0,0 +1,97 @@
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import httpStatus from "http-status";
+import { paginationFields } from "../../constants/pagination";
+import pick from "../../../shared/pick";
+import { SubscriptionHistoryService } from "./subscription-history.service";
+import { subscriptionHistoryFilterableFields } from "./subscription-history.constants";
+
+const createSubscriptionHistory = catchAsync(
+  async (req: Request, res: Response) => {
+    const result = await SubscriptionHistoryService.createSubscriptionHistory(
+      req.body
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Subscription history added successfully!",
+      data: result,
+    });
+  }
+);
+
+const getAllSubscriptionHistorys = catchAsync(
+  async (req: Request, res: Response) => {
+    const filters = pick(req.query, subscriptionHistoryFilterableFields);
+    const paginationOptions = pick(req.query, paginationFields);
+    const result = await SubscriptionHistoryService.getAllSubscriptionHistorys(
+      filters,
+      paginationOptions
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "All subscription historys fetched successfully!",
+      data: result,
+    });
+  }
+);
+
+const getSingleSubscriptionHistory = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result =
+      await SubscriptionHistoryService.getSingleSubscriptionHistory(id);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Subscription history fetched successfully!",
+      data: result,
+    });
+  }
+);
+
+const updateSubscriptionHistory = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const payload = req.body;
+    const result = await SubscriptionHistoryService.updateSubscriptionHistory(
+      id,
+      payload
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Subscription history updated in successfully!",
+      data: result,
+    });
+  }
+);
+const deleteSubscriptionHistory = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await SubscriptionHistoryService.deleteSubscriptionHistory(
+      id
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Subscription history deleted in successfully!",
+      data: result,
+    });
+  }
+);
+
+export const SubscriptionHistoryController = {
+  createSubscriptionHistory,
+  getAllSubscriptionHistorys,
+  getSingleSubscriptionHistory,
+  updateSubscriptionHistory,
+  deleteSubscriptionHistory,
+};
---
+++ src/app/modules/subscription-history/subscription-history.route.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import validateRequest from "../../middlewares/validateRequest";
+import authRole from "../../middlewares/authRole";
+import { ENUM_USER_ROLE } from "../../enums/user";
+import { SubscriptionHistoryValidation } from "./subscription-history.validation";
+import { SubscriptionHistoryController } from "./subscription-history.controller";
+
+const router = Router();
+
+// create SubscriptionHistory
+router.post(
+  "/",
+  validateRequest(
+    SubscriptionHistoryValidation.createSubscriptionHistorySchema
+  ),
+  SubscriptionHistoryController.createSubscriptionHistory
+);
+
+// get all SubscriptionHistorys
+router.get("/", SubscriptionHistoryController.getAllSubscriptionHistorys);
+
+// get single SubscriptionHistory
+router.get("/:id", SubscriptionHistoryController.getSingleSubscriptionHistory);
+
+// update single SubscriptionHistory
+router.patch(
+  "/:id",
+  authRole(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(
+    SubscriptionHistoryValidation.updateSubscriptionHistoryZodSchema
+  ),
+  SubscriptionHistoryController.updateSubscriptionHistory
+);
+
+// delete SubscriptionHistory
+router.delete(
+  "/:id",
+  authRole(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  SubscriptionHistoryController.deleteSubscriptionHistory
+);
+
+export const SubscriptionHistoryRoutes = router;
